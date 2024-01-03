d1b9e8ae3a55cce9cdfd5f7f9eaa3bef5ac48f3a
onerafb <aftab7t7@gmail.com>
2024-01-03 11:46:41 +0530

    vercel

--- db.js
+++ db.js
@@ -1,16 +1,7 @@
 const Pool = require("pg").Pool;
 require("dotenv").config();
-const pool = new Pool({
-  // user: process.env.USERNAME,
-  // password: process.env.PASSWORD,
-  // host: process.env.HOST,
-  // port: process.env.DBPORT,
-  
-  user: "postgres",
-  password: "password",
-  host: "localhost",
-  port: 5432,
-  database: "todoapp",
-});
 
+const pool = new Pool({
+  connectionString: process.env.POSTGRES_URL + "?sslmode=require",
+});
 module.exports = pool;
